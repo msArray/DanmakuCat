caf176f70b50f1c63e609692ee3dcd84c5da8205
Shinono <85401098+K93MSY@users.noreply.github.com>
2022-09-07 23:14:28 +0900

    toolアプデv2
    意外と早くpreview機能が追加できたので
    使用ライブラリhttps://p5js.org/

---
+++ docs/tool/preview.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(600, 600);
+}
+
+function draw(){
+    background(0);
+    fill(5,255,255);
+    rect(300+($('#spx').val()*1)-($('#bsize').val()*1)/2, 300+($('#spy').val()*1)-($('#bsize').val()*1)/2, $('#bsize').val()*1, $('#bsize').val()*1);
+}
