b4d2766106eb60e953dabac192cb0728d76af4dd
K93MSY <masaya93@ogaki-tv.ne.jp>
2022-10-09 09:23:15 +0900

    toolアプデ　プレビューに複数表示できるようにした

--- docs/tool/preview.js
+++ docs/tool/preview.js
@@ -6,4 +6,10 @@ function draw(){
     background(0);
     fill(5,255,255);
     rect(300+($('#spx').val()*1)-($('#bsize').val()*1)/2, 300+(-1*($('#spy').val()*1)-($('#bsize').val()*1)/2), $('#bsize').val()*1, $('#bsize').val()*1);
+
+    let pree = "["+$('#pren').val()+"]"
+    const obj = JSON.parse(pree);
+    for(let i=0;i<Object.keys(obj).length;i++){
+        rect(300+(obj[i].x*1)-(obj[i].size*1)/2, 300+(-1*(obj[i].y*1)-(obj[i].size*1)/2), obj[i].size*1, obj[i].size*1);
+    }
 }
--- docs/tool/script.js
+++ docs/tool/script.js
@@ -3,15 +3,27 @@ function build(){
         x:$('#spx').val()*1,
         y:$('#spy').val()*1,
         size:$('#bsize').val()*1,
-        checktime:$('#checktime').val()*1000,
-        poptime:$('#spawntime').val()*1000,
-        rmtime:$('#removetime').val()*1000,
+        checktime:$('#checktime').val()*1,
+        poptime:$('#spawntime').val()*1,
+        rmtime:$('#removetime').val()*1,
     }
     console.log(box)
     if($('#output').val()==""){
         $('#output').val(JSON.stringify(box))
     }else{
         $('#output').val($('#output').val()+','+JSON.stringify(box))
+    }
+}
+function previewadd(){
+    var prex = {
+        x:$('#spx').val()*1,
+        y:$('#spy').val()*1,
+        size:$('#bsize').val()*1,
+    }
+    if($('#pren').val()==""){
+        $('#pren').val(JSON.stringify(prex))
+    }else{
+        $('#pren').val($('#pren').val()+','+JSON.stringify(prex))
     }
 }
 function reset(){
